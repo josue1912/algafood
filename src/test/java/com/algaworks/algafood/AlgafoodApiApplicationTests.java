aa08cb617fc8a3d3dcfc6b2f948f8bcd298230cf
Josue Freitas <josue1912@gmail.com>
2022-09-28 08:41:37 -0300

    Adicionando classe de teste

--- src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgafoodApiApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 class AlgafoodApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
