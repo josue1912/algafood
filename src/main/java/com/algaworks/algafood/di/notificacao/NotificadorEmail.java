12ece81aaed02dd9db1933588a5b549a27256d81
Josue Freitas <josue1912@gmail.com>
2022-10-07 10:02:20 -0300

    Injetando beans spring

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -4,10 +4,15 @@ import com.algaworks.algafood.di.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NotificadorEmail {
+public class NotificadorEmail implements Notificador {
 
-  public void notificar(Cliente cliente, String mensagem){
-    System.out.printf("Notificando %s através do email %s: %s",
-        cliente.getNome(), cliente.getEmail(), mensagem);
+  public NotificadorEmail() {
+    System.out.println("NotificadorEmail");
+  }
+
+  @Override
+  public void notificar(Cliente cliente, String mensagem) {
+    System.out.printf("Notificando %s através do email %s: %s", cliente.getNome(),
+        cliente.getEmail(), mensagem);
   }
 }
